513d020ea72531af9699a317d4fce0df525b0275
funzin <nakazawa_fumito@abema.tv>
2018-05-16 11:07:50 +0900

    Initial commit

---
+++ AbemaAnimator/AbemaAnimator.h
@@ -0,0 +1,19 @@
+//
+//  AbemaAnimator.h
+//  AbemaAnimator
+//
+//  Created by 中澤 郁斗 on 2018/05/16.
+//  Copyright © 2018年 中澤 郁斗. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AbemaAnimator.
+FOUNDATION_EXPORT double AbemaAnimatorVersionNumber;
+
+//! Project version string for AbemaAnimator.
+FOUNDATION_EXPORT const unsigned char AbemaAnimatorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AbemaAnimator/PublicHeader.h>
+
+
