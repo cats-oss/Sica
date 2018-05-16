5f78fd25ce10774593305109563a13e6f7d45a99
funzin <nakazawa_fumito@abema.tv>
2018-05-16 11:09:49 +0900

    Add Demo

---
+++ Demo/Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Demo
+//
+//  Created by 中澤 郁斗 on 2018/05/16.
+//  Copyright © 2018年 中澤 郁斗. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
