049abca6c6ba81a69d0ce3fd8632de7614677fc2
funzin <nakazawa_fumito@abema.tv>
2018-05-21 11:11:12 +0900

    Fix sample

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -21,16 +21,13 @@ class ViewController: UIViewController {
 
         Animation: do {
             let animator = Animator(view: sampleView)
-            let v: [CGFloat] = [0, 12, 10, 4, 10, -22].map { $0 + sampleView.center.y }
-            let d = [0.5, 1.5, 0.2, 0.8, 0.2]
-            let timingFunctions: [TimingFunction] = [.easeOutExpo, .easeInOutCubic, .easeOutCubic, .easeInOutSine, .easeInQuint]
 
             animator.delay(4)
-                .addBasicAnimation(keyPath: .positionY, from: v[0], to: v[1], duration: d[0], timingFunction: timingFunctions[0])
-                .addBasicAnimation(keyPath: .positionY, from: v[1], to: v[2], duration: d[1], timingFunction: timingFunctions[1])
-                .addBasicAnimation(keyPath: .positionY, from: v[2], to: v[3], duration: d[2], timingFunction: timingFunctions[2])
-                .addBasicAnimation(keyPath: .positionY, from: v[3], to: v[4], duration: d[3], timingFunction: timingFunctions[3])
-                .addBasicAnimation(keyPath: .positionY, from: v[4], to: v[5], duration: d[4], delay: 0.7, timingFunction: timingFunctions[4])
+                .addBasicAnimation(keyPath: .positionY, from: 368, to: 380, duration: 0.5, timingFunction: .easeOutExpo)
+                .addBasicAnimation(keyPath: .positionY, from: 380, to: 378, duration: 1.5, timingFunction: .easeInOutCubic)
+                .addBasicAnimation(keyPath: .positionY, from: 378, to: 372, duration: 0.2, timingFunction: .easeOutCubic)
+                .addBasicAnimation(keyPath: .positionY, from: 372, to: 378, duration: 0.8, timingFunction: .easeInOutSine)
+                .addBasicAnimation(keyPath: .positionY, from: 378, to: 346, duration: 0.2, delay: 0.7, timingFunction: .easeInQuint)
                 .addSpringAnimation(keyPath: .boundsSize, from: CGSize(width: 0, height: 0), to:  CGSize(width: 240, height: 240), damping: 12, mass: 1, stiffness: 240, initialVelocity: 0, duration: 1)
                 .run(type: .sequence) {
                     print("Animation Done")
