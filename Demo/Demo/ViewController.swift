2e65a459759df4b072fe49078d2c81fe14667065
funzin <nakazawa_fumito@abema.tv>
2018-05-21 11:09:03 +0900

    Remove unnecessary code

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -36,13 +36,5 @@ class ViewController: UIViewController {
                     print("Animation Done")
             }
         }
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
