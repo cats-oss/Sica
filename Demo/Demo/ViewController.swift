ffd7fe70ed3ba23e172f7d568ac22668995ba641
funzin <nakazawa_fumito@abema.tv>
2018-05-21 10:48:12 +0900

    Fix parameter position

--- Demo/Demo/ViewController.swift
+++ Demo/Demo/ViewController.swift
@@ -30,7 +30,7 @@ class ViewController: UIViewController {
                 .addBasicAnimation(keyPath: .positionY, from: v[1], to: v[2], duration: d[1], timingFunction: timingFunctions[1])
                 .addBasicAnimation(keyPath: .positionY, from: v[2], to: v[3], duration: d[2], timingFunction: timingFunctions[2])
                 .addBasicAnimation(keyPath: .positionY, from: v[3], to: v[4], duration: d[3], timingFunction: timingFunctions[3])
-                .addBasicAnimation(keyPath: .positionY, from: v[4], to: v[5], delay: 0.7, duration: d[4], timingFunction: timingFunctions[4])
+                .addBasicAnimation(keyPath: .positionY, from: v[4], to: v[5], duration: d[4], delay: 0.7, timingFunction: timingFunctions[4])
                 .addSpringAnimation(keyPath: .boundsSize, from: CGSize(width: 0, height: 0), to:  CGSize(width: 240, height: 240), damping: 12, mass: 1, stiffness: 240, initialVelocity: 0, duration: 1)
                 .run(type: .sequence) {
                     print("Animation Done")
